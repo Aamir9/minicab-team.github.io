f38cf0c860ab814e71b4570a7bd2d250a2d340fc
AAMIR HUSAIN <malikaamir966@gmail.com>
2021-01-23 20:42:00 +0500

    hhh

---
+++ js/gainQuote.js
@@ -0,0 +1,30 @@
+﻿
+$(document).ready(function () {
+
+    $('.iconpick ,.pck').click(function () {
+
+        $('#from_places').val('');
+        $('#GetQuote').show();
+        $('#BookNow').hide();
+      
+        $('#LoginPopShow').hide();
+        $('#priceMileMain').hide();
+        $('#Prices').html('');
+
+ });
+
+
+    $('.icondrop ,.drp').click(function () {
+
+        $('#to_places').val('');
+        $('#GetQuote').show();
+        $('#BookNow').hide();
+        $('#LoginPopShow').hide();
+        $('#priceMileMain').hide();
+        $('#Prices').html('');
+
+   });
+
+
+
+});
